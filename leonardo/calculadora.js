3043f396f3783cd7680ba46564396e65469c3af9
leotrierveiler <leotrierveiler65@hotmail.com>
2022-05-20 17:47:30 -0300

    20/05/2022

---
+++ leonardo/calculadora.js
@@ -0,0 +1,38 @@
+// ATIVIDADE 8
+function realizarOperacao() {
+    let valor1 = document.getElementById ('v1').value;
+    let valor2 = document.getElementById ('v2').value;
+    let operacao = document.getElementById ('calculo').value;
+    valor1 = parseFloat (valor1);
+    valor2 = parseFloat (valor2);
+    let resultado = '';
+    switch (operacao) {
+        case "soma":
+            resultado = valor1 + valor2;
+            // alert('A operação realizada foi de ' + operacao + ' entre os dados ' + valor1 + ' e ' + valor2 + ' com seu resultado: ' + resultado + '!');
+            document.getElementById ('resposta').value += resultado;
+            break;
+        
+        case "subtração":
+            resultado = valor1 - valor2;
+            // alert('A operação realizada foi de ' + operacao + ' entre os dados ' + valor1 + ' e ' + valor2 + ' com seu resultado: ' + resultado + '!');
+            document.getElementById ('resposta').value += resultado;
+            break;
+
+        case "multiplicação":
+            resultado = valor1 * valor2;
+            // document.write ('A operação realizada foi de ' + operacao + ' entre os dados ' + valor1 + ' e ' + valor2 + ' com seu resultado: ' + resultado + '!');
+            document.getElementById ('resposta').value += resultado;
+            break;
+
+        case "divisão":
+            resultado = valor1 / valor2;
+            // document.write ('A operação realizada foi de ' + operacao + ' entre os dados ' + valor1 + ' e ' + valor2 + ' com seu resultado: ' + resultado + '!');
+            document.getElementById ('resposta').value += resultado;
+            break;  
+
+        default:
+            document.write ('Informe valores e uma operação válidos!');
+            break;
+    }
+}
---
+++ leonardo/salario.js
@@ -0,0 +1,20 @@
+function verificaSalario() {
+    let base = 2000;
+    let gerente = base + (base*10/100);
+    let supervisor = base + (base*50/100);
+    let funcionario = document.getElementById ('funcao').value;
+    switch (funcionario) {
+        case "gerente":
+            // alert ('Salário de: R$' + gerente);
+            document.getElementById ('resultado').value += ('R$' + gerente) ;
+            break;
+        case "supervisor":
+            // alert ('Salário de: R$'+ supervisor);
+            document.getElementById ('resultado').value += ('R$' + supervisor) ;
+            break;
+        default:
+            // alert ('Salário de: R$' + base);
+            document.getElementById ('resultado').value += ('R$' + base) ;
+            break;
+    }
+}
