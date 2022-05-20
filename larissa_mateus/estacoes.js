2d8b500b51d56c753c985d5564d9b6419cc1740e
MateusAugustoFerreira <mateusaugustoferreira@gmail.com>
2022-05-20 17:49:10 -0300

    larissa_mateus20/05

---
+++ larissa_mateus/estacoes.js
@@ -0,0 +1,38 @@
+// let mes = "novembro";
+
+// switch (mes) {
+//     case "dezembro":
+//     case "janeiro":
+//     case "fevereiro":
+//         console.log("O mes escolhido foi: " + mes + " referente ao verão ")    
+        
+//         break;
+
+//     case "marco":
+//     case "abril":
+//     case "maio":
+//         console.log("O mes escolhido foi: " + mes + " referente ao outono ")
+
+//         break;
+
+//     case "junho":
+//     case "julho":
+//     case "agosto":
+//         console.log("O mes escolhido foi: " + mes + " referente ao inverno ")
+        
+//         break;
+
+//     case "setembro":
+//     case "outubro":
+//     case "novembro":
+//         console.log("O mes escolhido foi: " + mes + " referente a primavera ")
+
+//         break;                               
+                
+
+//     default:
+//         console.log("outro mês")
+//         break;
+// }
+
+
---
+++ mateus-larissa/calculadora.js
@@ -0,0 +1,27 @@
+// let operacoes = "multiplicacao";
+// let valor1 = 10
+// let valor2 = 5 
+// let resultado 
+
+// switch (operacoes) {
+//     case "soma":
+//         resultado = valor1 + valor2
+//         console.log("A " + operacoes + " " + valor1 + " "  + valor2 + " é igual a" + resultado);
+//         break;
+//     case "subtracao":
+//         resultado = valor1 - valor2
+//         console.log("A " + operacoes + " de " + valor1 + " " + valor2 + " " + "é igual a" + resultado);
+//         break;
+//     case "divisao":
+//         resultado = valor1 / valor2 
+//         console.log ("A" + operacoes + " de " + valor1 + " " + valor2 + " " + " é igual a " + resultado);
+//         break;
+//     case "multiplicacao":
+//         resultado = valor1 * valor2
+//         console.log("A" + operacoes + " de " + valor1 + " " + valor2 + " " + " é igual a " + resultado);
+
+//         break;
+
+//     default:
+//         break;
+// }
