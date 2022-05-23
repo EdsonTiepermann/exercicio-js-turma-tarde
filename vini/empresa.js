0888ab9c544c1acee77f93a0c23888038bf74c3b
JulioVini <playeroneg3@gmail.com>
2022-05-23 17:53:38 -0300

    exercicios

--- vini/empresa.js
+++ vini/empresa.js
@@ -1,13 +1,19 @@
-let cargo = ""
-let salario ;
+let cargo = "supervisor"
+let salario = 2000;
 
 switch (cargo) {
     case "gerente":
-        salario = 
-        console.log();
+        salario = (salario * 0.10) + salario;
+        console.log(salario);
         
+        break;
+    case "supervisor":
+        salario = (salario * 0.50) + salario;
+        console.log(salario);
+            
         break;
 
     default:
         break;
+
 }
---
+++ vini/imc.js
@@ -0,0 +1,35 @@
+let peso = "60"
+let altura = "1.70"
+let imc = peso / (altura * altura)
+console.log(imc);
+
+if (imc < 18.5) {
+    console.log("Abaixo do peso");
+} else {
+    
+}
+if (imc > 18.5 < 24.9) {
+    console.log("Peso normal");
+} else {
+     
+}
+if (imc > 24.9 < 29.9) {
+    console.log("Sobrepeso");
+} else {
+     
+}
+if (imc < 30 > 34.9) {
+    console.log("Obesidade grau I");
+} else {
+    
+}
+if (imc < 35 > 39.9) {
+    console.log("Obesidade grau II");
+} else {
+    
+}
+if (imc < 35 > 40) {
+    console.log("Obesidade Morbida");
+} else {
+    
+}
