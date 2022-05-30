eb2f78930b8b2a3908ce223278895ff44724cdb0
DarkNtj <rrayssonvicente@gmail.com>
2022-05-30 17:50:47 -0300

    lyara_raysson_js

---
+++ animais.js
@@ -0,0 +1,18 @@
+let capivara = {
+    altura:  "60cm",
+    peso: "7 toneladas",
+    habitat: "floresta",
+    cor: "vermelha",
+    especie: "mamífero",
+
+    andar:function () {
+        console.log("pak pak pak pak")
+    },
+    comer:function () {
+        console.log("nhac nhac nhac")
+    },
+    correr:function (){
+        console.log("zunnnn.. zunnnnn.. iunnnnnn")
+    }
+}
+console.log(" A capivara tem " + capivara.altura + " pesa " + capivara.peso + " seu habitat natural é " + capivara.habitat + " da cor " + capivara.cor + " da espécie " + capivara.especie)
