36bf25203e43027c707b48e314ac90442d3dc3c2
Maribel37Garcia <maribelgarcia311285@gmail.com>
2022-06-03 17:48:36 -0300

    exercícios

---
+++ Maribel/algoritmos.js
@@ -0,0 +1,20 @@
+//   let resultado=0;
+
+// for(let i=100; i <200; i++){
+//     resultado=i%2
+//     if(resultado==1){
+//         console.log(i)
+//     }
+
+
+
+
+// let tabuada=3;
+// let resultado=0;
+
+// for(let i=1; i<10; i++){
+//     resultado=tabuada*i
+//     console.log(tabuada+"x"+i+"="+resultado)
+// }
+
+
