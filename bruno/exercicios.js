36fd4a66d456277f29ae85fb85dae3f181f1bf5b
brunoforna <bruno.fornazari3@gmail.com>
2022-06-03 17:47:51 -0300

    exerciciojs

---
+++ bruno/exercicios.js
@@ -0,0 +1,72 @@
+// let Alunos = ['Bruno', 'Larissa', 'Matheus', 'Leonardo']
+// console.log(Alunos) ;
+
+// Alunos.push('Maribel')
+// // console.log(Alunos)
+
+// let cachorro = {
+//     raca:'Pitbull' ,
+//     cor:'Marrom' ,
+//     sexo:'Macho' ,
+//     peso:'15kg' ,
+//     tamanho:'45cm' ,
+    
+//     latir:function() {
+//        console.log('au au au')
+//     },
+
+//     rosnando:function() {
+//         console.log('rrrr rrrr rrrr')
+//     },
+// }
+// cachorro.latir();
+// cachorro.rosnando();
+
+
+//  let for ( i = 100; i < 200; i++ ) {
+//     if((i 200 % 2) = 100) 
+// }
+// console.log
+
+
+// 07 - Faça um algoritmo que leia 6 valores positivos. Calcule a quantidade de números pares e ímpares, a média de valores pares e a média geral dos números lidos.[array]
+// let numeros = [6, 7, 17, 15, 3, 4];
+// let resto = 0; 
+// let totalPar = 0; 
+// let totalImpar = 0;
+// let mediaPar = 0;
+// let somaPares = 0;
+// let somaImpares = 0;
+
+// for (let index = 0; index < numeros.length; index++) {
+//     resto = numeros[index] % 2;
+//     if (resto == 0 ){
+//         totalPar++;
+//         somaPares = somaPares + numeros[index]; 
+
+//     } else {
+//         totalImpar++;
+//         somaImpares = somaImpares + numeros[index];
+
+//     }
+    
+//     mediaPar = somaPares / totalPar;
+
+//     mediaGeral =(somaPares + somaImpares ) / numeros.length;
+
+//     console.log(mediaGeral);
+
+// }
+
+
+// 02 - Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se encontram no conjunto dos números de 	    1 até 500.
+
+// let numeros = [3, 9, 15,] {
+//     for(=1; num<=500; index++ )
+//     if(num % 3 == 0 )
+
+// }
+// console.log()
+
+// 03 - Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e calcule a tabuada de N. Mostre a tabuada na forma: 0 x N = 0, 1 x N = 1N, 2 x N = 2N, ..., 10 x N = 10N
+
