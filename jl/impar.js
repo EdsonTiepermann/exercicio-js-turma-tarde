a57fd4543da8ac2fdcbc2e86a4bf1328d1df6112
LuizDani <lau1972.lds@gmail.com>
2022-06-06 17:49:49 -0300

    jl

---
+++ jl/impar.js
@@ -0,0 +1,10 @@
+numeros = 0;
+totalimpar = 0;
+
+for (let numeros = 1; numeros < 201; numeros++)
+
+    if (numeros % 2 != 0) {
+        totalimpar++
+        console.log(" O valor é " + numeros );
+    }
+console.log(numeros)
