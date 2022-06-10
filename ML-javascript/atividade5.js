aaa97e6ce282b4e88f2d06810f6dd32e2e021816
lucasruan05 <lucasruan2602@gmail.com>
2022-06-10 17:46:49 -0300

    atividades continuação

--- ML-javascript/atividade5.js
+++ ML-javascript/atividade5.js
@@ -1,3 +1,10 @@
-// Desenvolver um algoritmo que leia a altura(em cm) de 15 pessoas. Este programa deverá calcular e mostrar : [array] a. 
-// A menor altura do grupo; b. A maior altura do grupo;
-let altura = 0;
+// Desenvolver um algoritmo que leia a altura(em cm) de 15 pessoas. Este programa deverá calcular e mostrar : 
+// [array] a. A menor altura do grupo; b. A maior altura do grupo;
+const arr = [1.50 , 1.65 , 1.72 , 1.81 , 1.42 , 1.73]
+
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+
+console.log( + min + "cm");
+console.log( + max + "cm");
+
